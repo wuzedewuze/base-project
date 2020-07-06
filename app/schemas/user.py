0e6b5e0401465ca6ea95391587f2d1994c7127a3
wuyang <wuyang_229@126.com>
2020-07-06 03:43:22 -0700

    修改一些注释

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, EmailStr
 from datetime import date
 
 
+# 用户的公共基础类
 class UserBase(BaseModel):
     username: str
     email: Optional[EmailStr] = None
@@ -13,22 +14,31 @@ class UserBase(BaseModel):
     dis_active_time: date = None
 
 
+# 通过AIP接受的创建属性
 class UserCreate(UserBase):
     email = EmailStr
     password = Optional[str] = None
 
 
+# 通过AIP接受的更新属性
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+# 入库基础类
 class UserInDBBase(UserBase):
     id: Optional[int] = None
 
-    class Config:
+    class Config:  # 既可以使用对象又可以使用dict
         orm_mode = True
 
 
+# 通过AIP返回的附加属性
 class User(UserInDBBase):
     pass
 
 
+# 存储到数据库中的附加属性
 class UserInDB(UserInDBBase):
     hashed_password: str
 
